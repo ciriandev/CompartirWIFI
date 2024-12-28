a855de88cf978cc3a297d9a1602f69cd6df6beaf
ciriandev <rar1602@gmail.com>
2024-12-28 21:15:14 +0100

    Changes to be committed: new file:   qr.py
    Primer commit del proyecto

---
+++ qr.py
@@ -0,0 +1,22 @@
+import qrcode
+import qrcode.constants
+
+data = {
+    "S": "MOVISTAR_7BB0",
+    "I": "WPA2-PSK",
+    "C": "3NS5tYuBb67BK3LMHKL9",
+}
+wifi = f"WIFI:S:{data['S']};T:{data['T']};P:{data['P']};;"
+qr = qrcode.QRCode(
+    version=1,
+    error_correction= qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data(wifi)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color = "black", back_color="white")
+
+img.save("CasaWIFI.png")
